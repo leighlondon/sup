831bcd4d17cfeeedc51fc52d396f41ca45c2e467
Leigh London <leigh.00.london@gmail.com>
2014-12-16 20:15:21 +1100

    Extract the usage string.

--- main.go
+++ main.go
@@ -4,17 +4,6 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
-const usage = `superb.
-
-Usage:
-  sup <key> <value>
-  sup [options]
-
-Options:
-  -h --help	  Show this screen.
-  -v --version	  Show the version.
-`
-
 func main() {
 
 	arguments, _ := docopt.Parse(usage, nil, false, Version, false)
---
+++ usage.go
@@ -0,0 +1,12 @@
+package main
+
+const usage = `superb.
+
+Usage:
+  sup <key> <value>
+  sup [options]
+
+Options:
+  -h --help   Show this screen.
+  -v --version    Show the version.
+`
