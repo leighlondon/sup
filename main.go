b591369b06bfa8a69230bab49f6a0982db0bb7f5
Leigh London <leigh.00.london@gmail.com>
2014-12-18 18:02:17 +1100

    Exit immediately if an option is present.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/docopt/docopt-go"
 )
 
@@ -8,9 +10,13 @@ func main() {
 
 	arguments, _ := docopt.Parse(usage, nil, false, "", false)
 
+	// The options flags are only one at a time each, and
+	// should exit immediately when completed.
 	if arguments["--help"] == true {
 		printWarn(usage)
+		os.Exit(0)
 	} else if arguments["--version"] == true {
 		printWarn(VersionString)
+		os.Exit(0)
 	}
 }
