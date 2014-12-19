9596305ba0432a6b5d23beaf873504e8260317c7
Leigh London <leigh.00.london@gmail.com>
2014-12-20 10:36:20 +1100

    Comments.
    Adding comments to the (higher level) storage functions.

--- storage.go
+++ storage.go
@@ -6,15 +6,17 @@ import (
 	"os"
 )
 
+// Load the data stored in a file.
 func load(filename string) map[string]string {
-
+	// Load the data in as bytes, and then into a data structure.
 	bytes := readFile(filename)
 	data := loadItems(bytes)
-
 	return data
 }
 
+// Save the data to a file.
 func save(filename string, data map[string]string) {
+	// Marshal the data structure into bytes, and then save to a file.
 	bytes := makeBytes(data)
 	saveFile(filename, bytes)
 }
