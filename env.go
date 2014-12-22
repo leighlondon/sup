9c60366ffd0ad52b21f8b09f010f647726d61cdb
Leigh London <leigh.00.london@gmail.com>
2014-12-22 12:19:37 +1100

    Comments.
    Adding comments on the environment variables and default filename.

--- env.go
+++ env.go
@@ -4,9 +4,15 @@ import (
 	"os"
 )
 
+// The default filename to fall back to.
 const supFile = ".superb"
+
+// The environment variable for the filename.
 const fileEnv = "SUPERB_FILE"
 
+// Get the filename to be used for the storage.
+// This will check for an environment variable (SUPERB_FILE) and falls
+// back to a default filename if this variable is not set.
 func getFilename() string {
 
 	var name string
