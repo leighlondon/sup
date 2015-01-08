aef621df82d43ca7a835995e971170b34eb3f3f3
Leigh London <leigh.00.london@gmail.com>
2015-01-08 23:08:34 +1100

    Comments.
    More of them.

--- console.go
+++ console.go
@@ -7,14 +7,17 @@ import (
 	"strings"
 )
 
+// Printing to standard output.
 func printOut(s string) {
 	fmt.Fprintf(os.Stdout, fixNewline(s))
 }
 
+// Printing to standard error.
 func printWarn(s string) {
 	fmt.Fprintf(os.Stderr, fixNewline(s))
 }
 
+// Normalise any leading and trailing whitespace.
 func fixNewline(s string) string {
 	output := strings.TrimSpace(s)
 	return output + "\n"
