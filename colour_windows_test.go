00ce489d13315c0eb5b0fa28d9cf591735971472
Leigh London <leigh.00.london@gmail.com>
2015-01-24 10:59:25 +1100

    Test Windows with an invalid colour.
    Make sure "invalid" colour name requests don't modify the string at all.

--- colour_windows_test.go
+++ colour_windows_test.go
@@ -17,3 +17,15 @@ func TestValidColourOnWindowsDoesNothing(t *testing.T) {
 		t.Error("windows coloured string wasn't identical")
 	}
 }
+
+func TestInvalidColourOnWindowsDoesNothing(t *testing.T) {
+	// Attempting to wrap with an invalid colour on Windows should not
+	// change the string in any way.
+	testString := "test"
+	unColoured := wrapColour("blurple", testString)
+
+	// The strings should be literally identical.
+	if testString != unColoured {
+		t.Error("windows colour code wasn't base string")
+	}
+}
