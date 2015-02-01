fa06ecf4d9796eadbce1b328c783ee20a990bb89
Leigh London <leigh.00.london@gmail.com>
2015-02-01 12:53:35 +1100

    Update the usage options.
    Add a "delete" option flag for removing a key from storage.

--- usage.go
+++ usage.go
@@ -4,12 +4,12 @@ package main
 const usage = `superb.
 
 Usage:
-  sup <key>
-  sup <key> <value>
   sup [options]
+  sup [options] <key> [<value>]
 
 Options:
   -a --all        Show all of the keys and values.
+  -d --delete     Delete the listed key.
   -f --file       Show the storage file path.
   -h --help       Show this screen.
   -v --version    Show the version.
