5a415b45ea2de58f27347f9b6f345ae8dd2f50c8
Leigh London <leigh.00.london@gmail.com>
2015-03-04 21:07:53 +1100

    Test the Save->Load loop in the storage engine.

--- storage_test.go
+++ storage_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"reflect"
 	"testing"
 )
@@ -20,4 +21,23 @@ func TestMapToBytesToMapProducesSameMap(t *testing.T) {
 	if reflect.DeepEqual(unmarshalled, data) == false {
 		t.Error("map not decoded correctly")
 	}
+}
+
+func TestSaveToLoadProducesSameData(t *testing.T) {
+	// Saving data and then loading it should produce the same data.
+
+	data := make(map[string]string)
+	data["hello2"] = "world2"
+	data["dolla"] = "dolla bills"
+
+	filename := "sup_test_filename"
+
+	Save(filename, data)
+	loaded := Load(filename)
+
+	if reflect.DeepEqual(data, loaded) == false {
+		t.Error("saved data different from loaded")
+	}
+
+	os.Remove(filename)
 }
