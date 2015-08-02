0530e1347fc7af5092d62bea03f7cba2b56aa7ba
Leigh London <leigh.00.london@gmail.com>
2015-08-02 22:56:49 +1000

    Test shouldn't rely on environment.

--- colour_unix_test.go
+++ colour_unix_test.go
@@ -11,7 +11,7 @@ func TestColouredStingsHaveEscapeCodes(t *testing.T) {
 	// Attempting to wrap with a valid colour should wrap the string
 	// in a valid ANSI escape code.
 	testString := "test"
-	coloured := WrapColour("red", testString)
+	coloured := colourText("red", testString)
 
 	if !strings.Contains(coloured, "\x1b") {
 		t.Error("didn't contain valid color code")
@@ -22,7 +22,7 @@ func TestInvalidColoursReturnTheSameString(t *testing.T) {
 	// Attempting to wrap with an invalid colour should just return
 	// the original string.
 	testString := "test"
-	coloured := WrapColour("blurple", testString)
+	coloured := colourText("blurple", testString)
 
 	if strings.Contains(coloured, "\x1b") {
 		t.Error("added an escape code incorrectly")
