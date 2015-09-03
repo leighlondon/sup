36e422af832a7f14b1127f77d2a66c0e7e25f863
Leigh London <leigh.00.london@gmail.com>
2015-09-03 19:30:53 +1000

    Remove the flags that aren't set up.

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ Usage:
     sup [options] <key> [<value>]
 
 Options:
-    -a --all        Show all of the keys and values.
-    -d --delete     Delete the listed key.
-    -f --file       Show the storage file path.
-    -h --help       Show this screen.
-    -v --version    Show the version.
+    -a     Show all of the keys and values.
+    -d     Delete the listed key.
+    -f     Show the storage file path.
+    -h     Show this screen.
+    -v     Show the version.
 `
 
 func main() {
