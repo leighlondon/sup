e4c0d123067eabc4e2d17bd3c2514bc8b9b3b5ee
Leigh London <leigh.00.london@gmail.com>
2015-09-26 22:07:48 +1000

    Remove the colour features.

--- colour.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// WrapColour wraps a provided string with ANSI colour codes, if colour
-// printing is possible on the platform.
-func WrapColour(colour, input string) string {
-	return wrapColour(colour, input)
-}
--- colour_windows.go
+++
@@ -1,9 +0,0 @@
-// +build windows
-
-package main
-
-func wrapColour(colour, input string) string {
-	// Windows builds don't have colour printing enabled, so
-	// calling the colour function is essentially a no-op.
-	return input
-}
