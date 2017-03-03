76a758fd0f6b12eec0f199a92808c9eb85c030e5
Leigh London <leigh.00.london@gmail.com>
2017-03-03 17:06:51 +1100

    Remove redundant test.
    Relies on a previous architecture.

--- storage_test.go
+++ storage_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"os"
 	"reflect"
 	"testing"
 )
@@ -13,27 +12,10 @@ func TestMapToBytesToMapProducesSameMap(t *testing.T) {
 	data["hello"] = "world"
 	data["message"] = "response"
 	// Round loop test.
-	marshalled := makeBytes(data)
+	marshalled, _ := makeBytes(data)
 	unmarshalled := loadItems(marshalled)
 	// Make sure that it's equal.
 	if reflect.DeepEqual(unmarshalled, data) == false {
 		t.Error("map not decoded correctly")
 	}
-}
-
-func TestSaveToLoadProducesSameData(t *testing.T) {
-	// Saving data and then loading it should produce the same data.
-	data := make(map[string]string)
-	data["hello2"] = "world2"
-	data["dolla"] = "dolla bills"
-	filename := "sup_test_filename"
-	// Save then load it.
-	SaveData(filename, data)
-	loaded := LoadData(filename)
-	// Make sure that it's equal.
-	if reflect.DeepEqual(data, loaded) == false {
-		t.Error("saved data different from loaded")
-	}
-	// Clear the temporary file.
-	os.Remove(filename)
 }
